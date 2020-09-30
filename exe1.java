4198220fba311bdb09d780844a01dffe6310643c
otaviopato <otaviopato2017@outlook.com>
2020-09-29 23:54:48 -0300

    print completo ex1!

--- exe1.java
+++ exe1.java
@@ -1,7 +1,7 @@
 class exe1{
     public static void main(String[] args) {
 
-        System.out.println("Programação \n  - Professor: Fernando Bevilacqua \n  - Aluno: Seu nome aqui \n  - Objetivo: \n   O objetivo dessa disciplina é compreender os conceitos fundamentais \n   do paradigma de programação orientada a objetos e aplicá-los no \n   desenvolvimento de soluções de software. \n - Turno: Vespertino");
+        System.out.println("Programação \n  - Professor: Fernando  Bevilacqua \n  - Aluno: Seu nome aqui \n  - Objetivo: \n   O objetivo dessa disciplina é compreender os conceitos fundamentais \n   do paradigma de programação orientada a objetos e aplicá-los no \n   desenvolvimento de soluções de software. \n - Turno: Vespertino");
         
     }
 }
