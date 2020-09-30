998bddef6d7202f4d70278b40906ea87fd87b998
otaviopato <otaviopato2017@outlook.com>
2020-09-30 00:11:35 -0300

    Exe2 completo

---
+++ exe2.java
@@ -0,0 +1,21 @@
+public class exe2 {
+    /**
+     *
+     */
+    public static void main(String[] args) {
+
+        int n1 = 23;
+        int n2 = 33;
+        double d1 = 23.33;
+        double d2 = 96.44;
+        
+        int result1 = n1 + n2;
+        double result2 = d1 + d2;
+
+        System.out.println(result1);
+        System.out.println(result2);
+        
+    }
+        
+    
+}
